4532aaf3e7b884a4b372f18be265077306d17d7c
Peter St.Jean <pstjean@mmm.com>
2021-07-14 19:58:10 -0400

    Avoid hand-editing migrations file

--- db/migrate/20210709002429_create_git_notes.rb
+++ db/migrate/20210709002429_create_git_notes.rb
@@ -1,10 +1,6 @@
 class CreateGitNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :git_notes do |t|
-      t.string :path     # example: "/path/to/file.rb"
-      t.integer :line    # starting line of note
-      t.integer :column  # starting column of note
-      t.text :body       # body of note
 
       t.timestamps
     end

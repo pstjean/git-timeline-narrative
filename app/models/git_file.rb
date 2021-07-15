aa567059fd59925d6cb8f242b33580f170f00e92
Peter St.Jean <pstjean@mmm.com>
2021-07-14 21:24:30 -0400

    Adding the file <-> note relationship

--- app/models/git_file.rb
+++ app/models/git_file.rb
@@ -1,3 +1,4 @@
 class GitFile < ApplicationRecord
   belongs_to :git_commit
+  has_many :git_notes, through: :file_notes
 end
--- app/models/git_note.rb
+++ app/models/git_note.rb
@@ -2,6 +2,7 @@ require 'rugged'
 
 class GitNote < ApplicationRecord
   belongs_to :git_commit
+  has_many :git_files, through: :file_notes
   # attr_reader :head, :repo
 
   # def initialize args

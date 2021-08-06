433c7672d19985f645dbb0c0ca9666f2857a58c4
jordancolbeth <jordan@scitools.com>
2021-08-06 09:38:53 -0600

    Rerouted root path to git repository index view, added buttons to add and delete actions for git repositories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'git_repositories#index'
 
   # nested resource git_commits under git_repositories
   resources :git_repositories, only: [:index, :show, :new, :create, :destroy] do

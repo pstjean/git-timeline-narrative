24409e398bbb360e9ecc4b17aa8d51e144efe292
Peter St.Jean <pstjean@mmm.com>
2021-08-09 18:56:56 -0400

    Standard Rails form for file notes

--- app/controllers/file_notes_controller.rb
+++ app/controllers/file_notes_controller.rb
@@ -3,6 +3,10 @@ class FileNotesController < ApplicationController
   end
 
   def create
+    @file_note = FileNote.create(file_note_params)
+    unless @file_note.valid?
+      puts @file_note.errors.full_messages
+    end
   end
 
   def edit
@@ -13,4 +17,13 @@ class FileNotesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def file_note_params
+      whitelisted_params = params.require(:file_note).permit(:comment, :git_file_id, file_line_ids:[])
+      whitelisted_params[:git_note_id] = params.require(:git_note_id)
+      puts "WHITELISTED"
+      puts whitelisted_params
+      whitelisted_params
+    end
 end
--- app/controllers/git_commits_controller.rb
+++ app/controllers/git_commits_controller.rb
@@ -5,5 +5,6 @@ class GitCommitsController < ApplicationController
   def show
     @commit = GitCommit.find(params[:id])
     @files = GitFile.where(git_commit: @commit)
+    @git_note = GitNote.find_or_create_by(git_commit: @commit)
   end
 end
